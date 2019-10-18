18d0597a0bc8eb87d492cdf19c7f642ea24a9911
Liam McKay <lmckay@msg.ucsf.edu>
2019-10-18 14:58:34 -0700

    removed comments

--- pregui_img_analysis_3.py
+++ pregui_img_analysis_3.py
@@ -67,86 +67,17 @@ def draw_circles_on_image_center(image,cx,cy,radii,colour,dotsize):
 def save_canny_save_fit(path,sig,low,high): #sig=3,low = 0, high = 30
     zstack = io.imread(path)  
     image = img_as_ubyte(zstack[:,:,0]) # finds the top x-y pixels in the z-stack
-    
-    #mask = image > 20
-    #fprefix = os.path.basename(path).strip('.jpg')
-    #bn = fprefix + '_CANNY.jpg'
-    #current_dir = os.getcwd()
-    #cannysavepath = current_dir + '/' + plate_dir.strip('/') + '_canny/' + bn
-
     edges = canny(image,sigma=sig,low_threshold=low,high_threshold=high)   # edge detection
-
-    #plt.imsave(fname=str(p + fprefix + '_ubyte.jpg'), arr=image)
-    #plt.imsave(fname=str(p + fprefix + '_bounded.jpg'), arr=mask)
-    #bn = fprefix + '_dropfinder2.jpg'
-    #fitsavepath = current_dir + '/' + plate_dir.strip('/') + '_fit/' + bn
-    #try:
-        #plt.imsave(fname=cannysavepath, arr=edges)
-        #plt.close()
-    #except FileNotFoundError:
-        #print('FileNotFoundError: making ' + plate_dir.strip('/') + '_canny')
-        #os.mkdir(current_dir + '/' + plate_dir.strip('/') + '_canny/')
-        #print('writing ' + cannysavepath)
-        #plt.imsave(fname=cannysavepath, arr=edges)
-        #plt.close()
-    #print(cannysavepath)
-
-
-
     accum_d, cx_d, cy_d, radii_d = circular_hough_transform(135,145,2,edges,1) #edge detection on drop, params: r1,r2,stepsize,image,peaknum. Key params to change are r1&r2 for start & end radius
-    ### This works well for echo RT plate type for rockmaker
-
-
     ### room temp well
     accum_w, cx_w, cy_w, radii_w = circular_hough_transform(479,495,1,edges,1) #edge detection on well. Units for both are in pixels
     ### This works well for echo 4C plate type for rockmaker
     accum_w, cx_w, cy_w, radii_w = circular_hough_transform(459,475,1,edges,1) #edge detection on well. Units for both are in pixels
 
-    #try:
-        #im = image
-        #padsize = np.array([(0,100),(0,100)])
-        #pad_im = pad(image,padsize,mode='constant')
-        #fig, ax = plt.subplots(nrows=1,ncols=1,figsize=(20,8))
-        #draw_circles_on_image(pad_im,cx_d,cy_d,radii_d,255,2)
-        #draw_circles_on_image(pad_im,cx_w,cy_w,radii_w,255,2)
-
-        ### Code to draw the inner circle
-        #r_inner = 0.73701*radii_w
-        #r_inner = r_inner.astype(int)
-        #draw_circles_on_image(pad_im,cx_w,cy_w,r_inner,255,2)
-
-        
-        #cx=641
-        #cy=553
-        #rad=487
-        #c, r = circle_perimeter(cx, cy,rad)
-        #pad_im[r,c] = 255
-        #pad_im[cx-2:cx+2,cy-2:cy+2]=255
-
-        #try:
-            #plt.imsave(fname=fitsavepath, arr=pad_im,cmap='Greys_r')
-            #plt.close()
-        #except FileNotFoundError:
-            #print('making ' + plate_dir.strip('/') + '_fit/')
-            #os.mkdir(current_dir + '/' + plate_dir.strip('/') + '_fit/')
-            #print('writing' + bn)
-            #plt.imsave(fname=fitsavepath, arr=pad_im,cmap='Greys_r')
-            #plt.close()
-        #print(fitsavepath)
-    #except IndexError:
-        #print('IndexError' + ' ' + fitsavepath)
-    #plt.imshow(pad_im)
-    #print(path)
     return cx_d,cy_d,radii_d, cx_w, cy_w, radii_w
 
-### This will no longer be called because we will be working with overlayed images: JTB
-def find_image_paths():
-    my_paths = glob.glob('plateID_9792/*/wellNum_*/profileID*') #The plate ID could be a sys.argv
-    my_paths.sort(key=lambda x: (int(x.split('wellNum_')[1].split('/')[0])))
-    return my_paths
     
 def main():
-    # argument is plateID_****
 
     t0=time.time()  ### save time to know how long this script takes (this one takes longer than step 2)
 
@@ -157,23 +88,9 @@ def main():
     
     current_directory = os.getcwd()
     plate_dir = sys.argv[1]
+    plate_temperature = sys.argv[2]
     
-    ### Aji's code for the not overlayed images
-# my_paths = find_image_paths() #The path string object
-#  image_list = [] #empty holder list
-    # test#,well#,d#
-    #Save format t1w1d1,t2w1d1
-    ##gets only the images
-#  for path in my_paths: #
-#    print(str(path))
-#    images = glob.glob(path + '/*ef*')
-#    images.sort(key=lambda x: int(x.split('_r')[1].split('_ef')[0]))
-        
-#    if len(images)==2:
-#      image = images[1]
-#      image_list.append(image)
 
-    ### Justin's code to run on the overlayed images
     image_list=glob.glob("{}/overlayed/*".format(plate_dir))
     image_list.sort(key=lambda x: (int(x.split('well_')[1].split('_overlay')[0].split("_subwell")[0])))
     
@@ -183,18 +100,9 @@ def main():
     }
 
     print(current_directory + '/' + plate_dir.strip('/') + '_offsets.csv') ### eventually do this in the main gui
-
-    #f = open(current_directory + '/' + plate_dir.strip('/') + '_offsets.csv','wa')
-    #f.write('offset_x,offset_y,radii_d,radii_w,cx_d,cy_d,cx_w,cy_w\n')
     
     wellnames = Plate.well_names
     plate_to_pickle = Plate(Plate.plate_dict) #empty dictionary
-
-### Don't need incrementer n anymore with fix by JTB
-# n=0 
-    ### JTB added im_idx and enumerate to keep track of image
-    
-
 
     a = {}
     
@@ -219,7 +127,7 @@ def main():
     for im_idx, im_path in sorted(dict_image_path_subwells.items()):
         print("processing: ", im_idx, im_path)
         if im_path:
-            cx_d,cy_d,radii_d, cx_w, cy_w, radii_w = save_canny_save_fit(im_path,3,0,50) ### calling this function also saves
+            cx_d,cy_d,radii_d, cx_w, cy_w, radii_w = save_canny_save_fit(im_path,3,0,50,plate_temperature) ### calling this function also saves
 
         # cx_d,cy_d,radii_d, cx_w, cy_w, radii_w = [0,0,0,0,0,0] time saving code (will output zeros)
         ### radii radius of the drop circle 
